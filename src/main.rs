a982c1ab94b76265f18de0bc11e90f9cb3890943
InnocentusLime <innocentuslime.help@gmail.com>
2024-09-08 21:47:28 +0300

    Basic tilemap rendering

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use macroquad::prelude::*;
+use macroquad_tiled::*;
 
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum GameState {
     TitleScreen,
     LoadingSave,
@@ -8,10 +10,25 @@ pub enum GameState {
 
 #[macroquad::main("Silli RPG")]
 async fn main() {
+    set_default_filter_mode(FilterMode::Nearest);
+
+    let map = load_string("./assets/castle_test.tmj").await.unwrap();
+    let tileset = load_string("./assets/castle.tsj").await.unwrap();
+    let tileset_tex = load_texture("./assets/castle.png").await.unwrap();
+    let map = load_map(
+        &map,
+        &[("castle.png", tileset_tex)],
+        &[("castle.tsj", &tileset)],
+    ).unwrap();
+
     loop {
         clear_background(LIGHTGRAY);
 
         draw_text("Hey stinky", 0.0, 20.0, 16.0, RED);
+
+        map.draw_tiles("FloorWall", Rect::new(0.0, 0.0, 32.0 * 30.0, 32.0 * 20.0), None);
+        map.draw_tiles("Decor", Rect::new(0.0, 0.0, 32.0 * 30.0, 32.0 * 20.0), None);
+        map.draw_tiles("DecorObjs", Rect::new(0.0, 0.0, 32.0 * 30.0, 32.0 * 20.0), None);
 
         next_frame().await
     }
