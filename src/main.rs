7c472f43b9389654c64d2bc70a6f6483c76b68df
InnocentusLime <innocentuslime.help@gmail.com>
2024-09-13 23:36:45 +0300

    new render system

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod render;
 
 use macroquad::prelude::*;
 use macroquad_tiled::*;
-use render::{Render, RenderModel};
+use render::{Render, RenderModel, RenderTile};
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum GameState {
@@ -20,7 +20,7 @@ async fn main() {
     let tileset_tex = load_texture("./assets/castle.png").await.unwrap();
     let map = load_map(
         &map,
-        &[("castle.png", tileset_tex)],
+        &[("castle.png", tileset_tex.clone())],
         &[("castle.tsj", &tileset)],
     ).unwrap();
 
@@ -29,6 +29,8 @@ async fn main() {
     let mut render = Render::new();
 
     let mut pos = ivec2(9 * 32, 8 * 32);
+
+    let render_off = ivec2(0, -32);
 
     loop {
         clear_background(LIGHTGRAY);
@@ -46,14 +48,93 @@ async fn main() {
             pos.y += 1;
         }
 
-        draw_text("Hey stinky", 0.0, 20.0, 16.0, RED);
 
-        render.draw(&RenderModel {
-            map: Some(&map),
-            actors: &[
-                (pos, 2, RED, uvec2(16, 64)),
-            ],
+        let floor_layer = &map.layers["FloorWall"];
+        let objs_layer = &map.layers["DecorObjs"];
+        let tileset = &map.tilesets["castle"];
+        let tile_tex_rect = |id: u32| {
+            let x = (id % tileset.columns) * (tileset.tilewidth as u32);
+            let y = (id / tileset.columns) * (tileset.tileheight as u32);
+
+            Rect {
+                x: x as f32,
+                y: y as f32,
+                w: tileset.tilewidth as f32,
+                h: tileset.tileheight as f32,
+            }
+        };
+        let pos_iter = (0..floor_layer.height).flat_map(|y| {
+            (0..floor_layer.width).map(move |x| (x, y))
         });
+        render.draw_tiles(
+            pos_iter
+                .filter_map(|(x, y)| {
+                    let idx = (x + y * floor_layer.width) as usize;
+                    Some((
+                        (x, y),
+                        floor_layer.data[idx].as_ref()?
+                    ))
+                })
+                .map(|((x, y), tile)| RenderTile {
+                    z_order: 0,
+                    pos: ivec2(x as i32, y as i32) * 32,
+                    render_off: ivec2(0, 0),
+                    tex_rect: tile_tex_rect(tile.id),
+                }),
+            32.0,
+            &tileset_tex
+        );
+        let pos_iter = (0..objs_layer.height).flat_map(|y| {
+            (0..objs_layer.width).map(move |x| (x, y))
+        });
+        render.draw_tiles(
+            pos_iter
+                .filter_map(|(x, y)| {
+                    let idx = (x + y * objs_layer.width) as usize;
+                    Some((
+                        (x, y),
+                        objs_layer.data[idx].as_ref()?
+                    ))
+                })
+                .map(|((x, y), tile)| RenderTile {
+                    z_order: 0,
+                    pos: if tile.id == 146 || tile.id == 147 || tile.id == 148 {
+                        ivec2(x as i32, y as i32 - 1) * 32
+                    } else { ivec2(x as i32, y as i32) * 32 },
+                    render_off: if tile.id == 146 || tile.id == 147 || tile.id == 148 {
+                        ivec2(0, 32)
+                    } else { ivec2(0, 0) },
+                    tex_rect: tile_tex_rect(tile.id),
+                })
+                .chain(std::iter::once(RenderTile {
+                    z_order: 0,
+                    pos,
+                    render_off,
+                    tex_rect: Rect {
+                        x: 224.0,
+                        y: 544.0,
+                        w: 32.0,
+                        h: 64.0,
+                    },
+                })),
+            32.0,
+            &tileset_tex
+        );
+
+        draw_circle(
+            (pos.x) as f32,
+            (pos.y) as f32,
+            2.0,
+            RED
+        );
+
+        // render.draw(&RenderModel {
+        //     map: Some(&map),
+        //     actors: &[
+        //         (pos, 2, RED, uvec2(16, 64)),
+        //     ],
+        // });
+        draw_text(&format!("FPS: {}", get_fps()), 0.0, 20.0, 16.0, RED);
 
         next_frame().await
     }
--- src/render.rs
+++ src/render.rs
@@ -2,112 +2,144 @@ use macroquad::prelude::*;
 use macroquad_tiled::*;
 
 const TILE_SIZE: u32 = 32;
+
+#[derive(Debug, Clone, Copy)]
+pub struct RenderTile {
+    pub z_order: u8,
+    pub pos: IVec2,
+    pub render_off: IVec2,
+    pub tex_rect: Rect,
+}
+
 pub struct RenderModel<'a> {
     pub map: Option<&'a Map>,
     pub actors: &'a [(IVec2, u32, Color, UVec2)],
 }
 
 pub struct Render {
-    layer_buff: Vec<(u32, String)>,
-    actor_buff: Vec<(IVec2, u32, Color, UVec2)>,
+    tile_buffer: Vec<RenderTile>,
 }
 
 impl Render {
     pub fn new() -> Self {
         Self {
-            layer_buff: Vec::new(),
-            actor_buff: Vec::new(),
+            tile_buffer: vec![],
         }
+    }
+
+    pub fn draw_tiles(
+        &mut self,
+        tiles: impl Iterator<Item = RenderTile>,
+        tile_size: f32,
+        atlas: &Texture2D,
+    ) {
+        self.tile_buffer.clear();
+        self.tile_buffer.extend(tiles);
+        self.tile_buffer.sort_by_key(|tile| {
+            (tile.pos.y, tile.pos.x, tile.z_order)
+        });
+
+        self.tile_buffer.iter().for_each(|tile| {
+            let pos = (tile.pos + tile.render_off).as_vec2();
+            draw_texture_ex(
+                atlas,
+                pos.x,
+                pos.y,
+                WHITE,
+                DrawTextureParams {
+                    // dest_size: Some(vec2(tile_size, tile_size)),
+                    source: Some(tile.tex_rect),
+                    ..Default::default()
+                });
+        });
     }
 
     pub fn draw(&mut self, model: &RenderModel) {
-        if let Some(map) = model.map {
-            self.draw_map(map, model.actors);
-        }
+
     }
 
-    fn draw_map(&mut self, map: &Map, actors: &[(IVec2, u32, Color, UVec2)]) {
-        self.layer_buff.clear();
-        self.actor_buff.clear();
+    // fn draw_map(&mut self, map: &Map, actors: &[(IVec2, u32, Color, UVec2)]) {
+    //     self.layer_buff.clear();
+    //     self.actor_buff.clear();
 
-        self.actor_buff.extend(
-            actors.iter().map(|x| *x)
-        );
-        self.layer_buff.extend(map.raw_tiled_map.layers.iter()
-            .map(|layer| {
-                let idx = layer.properties.iter().find(|x| x.name == "layer_order")
-                            .map(|x| match &x.value {
-                                PropertyVal::UInt(x) => *x as u32,
-                                _ => panic!("Ouch")
-                            })
-                            .unwrap_or(0);
-                (idx, layer.name.clone())
-            })
-        );
+    //     self.actor_buff.extend(
+    //         actors.iter().map(|x| *x)
+    //     );
+    //     self.layer_buff.extend(map.raw_tiled_map.layers.iter()
+    //         .map(|layer| {
+    //             let idx = layer.properties.iter().find(|x| x.name == "layer_order")
+    //                         .map(|x| match &x.value {
+    //                             PropertyVal::UInt(x) => *x as u32,
+    //                             _ => panic!("Ouch")
+    //                         })
+    //                         .unwrap_or(0);
+    //             (idx, layer.name.clone())
+    //         })
+    //     );
 
-        self.layer_buff.sort_by_key(|&(idx, _)| idx);
-        self.actor_buff.sort_by_key(|&(pos, layer, _, size)| (pos.y as u32 + size.y, layer));
+    //     self.layer_buff.sort_by_key(|&(idx, _)| idx);
+    //     self.actor_buff.sort_by_key(|&(pos, layer, _, size)| (pos.y as u32 + size.y, layer));
 
-        let mut act_curr = 0;
-        let width = map.raw_tiled_map.width;
-        let height = map.raw_tiled_map.height;
-        let pos_iter = (0..height).flat_map(|y| {
-            (0..width).map(move |x| (x, y))
-        });
+    //     let mut act_curr = 0;
+    //     let width = map.raw_tiled_map.width;
+    //     let height = map.raw_tiled_map.height;
+    //     let pos_iter = (0..height).flat_map(|y| {
+    //         (0..width).map(move |x| (x, y))
+    //     });
 
-        for (layer_idx, layer_id) in &self.layer_buff {
-            for (x, y) in pos_iter.clone() {
-                let layer = &map.layers[layer_id];
+    //     for (layer_idx, layer_id) in &self.layer_buff {
+    //         for (x, y) in pos_iter.clone() {
+    //             let layer = &map.layers[layer_id];
 
-                if let Some(&(pos, layer, col, size)) = self.actor_buff.get(act_curr) {
-                    let mut not_draw = layer != *layer_idx;
-                    not_draw = not_draw || pos.y < 0;
-                    not_draw = not_draw || ((pos.y + size.y as i32) / map.raw_tiled_map.tileheight as i32) != y as i32;
+    //             if let Some(&(pos, layer, col, size)) = self.actor_buff.get(act_curr) {
+    //                 let mut not_draw = layer != *layer_idx;
+    //                 not_draw = not_draw || pos.y < 0;
+    //                 not_draw = not_draw || ((pos.y + size.y as i32) / map.raw_tiled_map.tileheight as i32) != y as i32;
 
-                    if !not_draw {
-                        // info!("({}, {}) -- ({}, {})",
-                        //     x, y,
-                        //     pos.x / map.raw_tiled_map.tilewidth as i32,
-                        //     pos.y / map.raw_tiled_map.tileheight as i32,
-                        // );
+    //                 if !not_draw {
+    //                     // info!("({}, {}) -- ({}, {})",
+    //                     //     x, y,
+    //                     //     pos.x / map.raw_tiled_map.tilewidth as i32,
+    //                     //     pos.y / map.raw_tiled_map.tileheight as i32,
+    //                     // );
 
-                        draw_rectangle(pos.x as f32, pos.y as f32, size.x as f32, size.y as f32, col);
+    //                     draw_rectangle(pos.x as f32, pos.y as f32, size.x as f32, size.y as f32, col);
 
-                        act_curr += 1;
-                    }
-                }
+    //                     act_curr += 1;
+    //                 }
+    //             }
 
-                if x < layer.width && y < layer.height {
-                    let tile = &layer.data[(x + y * layer.width) as usize];
-                    if let Some(tile) = tile {
-                        let tileset = &map.tilesets[&tile.tileset];
-                        let tx = (tile.id % tileset.columns) *
-                            (tileset.tilewidth as u32 + tileset.spacing as u32);
-                        let ty = tile.id / tileset.columns *
-                            (tileset.tileheight as u32 + tileset.spacing as u32);
-                        let tile_width = tileset.tilewidth as f32;
-                        let tile_height = tileset.tileheight as f32;
+    //             if x < layer.width && y < layer.height {
+    //                 let tile = &layer.data[(x + y * layer.width) as usize];
+    //                 if let Some(tile) = tile {
+    //                     let tileset = &map.tilesets[&tile.tileset];
+    //                     let tx = (tile.id % tileset.columns) *
+    //                         (tileset.tilewidth as u32 + tileset.spacing as u32);
+    //                     let ty = tile.id / tileset.columns *
+    //                         (tileset.tileheight as u32 + tileset.spacing as u32);
+    //                     let tile_width = tileset.tilewidth as f32;
+    //                     let tile_height = tileset.tileheight as f32;
 
-                        draw_texture_ex(
-                            &tileset.texture,
-                            x as f32 * tile_width,
-                            y as f32 * tile_height,
-                            WHITE,
-                            DrawTextureParams {
-                                dest_size: Some(vec2(tile_width, tile_height)),
-                                source: Some(Rect {
-                                    x: tx as f32,
-                                    y: ty as f32,
-                                    w: tile_width,
-                                    h: tile_height,
-                                }),
-                                ..Default::default()
-                            }
-                        );
-                    }
-                }
+    //                     draw_texture_ex(
+    //                         &tileset.texture,
+    //                         x as f32 * tile_width,
+    //                         y as f32 * tile_height,
+    //                         WHITE,
+    //                         DrawTextureParams {
+    //                             dest_size: Some(vec2(tile_width, tile_height)),
+    //                             source: Some(Rect {
+    //                                 x: tx as f32,
+    //                                 y: ty as f32,
+    //                                 w: tile_width,
+    //                                 h: tile_height,
+    //                             }),
+    //                             ..Default::default()
+    //                         }
+    //                     );
+    //                 }
+    //             }
 
-            }
-        }
-    }
+    //         }
+    //     }
+    // }
 }
